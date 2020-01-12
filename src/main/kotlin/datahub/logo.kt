3720690123a0cdbda72165d91cfe1a94bfa383ef
Jensen <suiterchik@gmail.com>
2020-01-12 23:30:09 +0800

    add logo

---
+++ src/main/kotlin/datahub/logo.kt
@@ -0,0 +1,27 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package datahub
+
+const val yellowBg = "\u001B[1;43;97m"
+const val greyBg = "\u001B[1;100;97m"
+const val end = "\u001B[m\n"
+const val logo = "\n" + greyBg + "  _____        _       " + yellowBg + " _           _      " + end +
+    greyBg + " |  __ \\      | |      " + yellowBg + "| |         | |     " + end +
+    greyBg + " | |  | | __ _| |_ __ _" + yellowBg + "| |__  _   _| |__   " + end +
+    greyBg + " | |  | |/ _` | __/ _` " + yellowBg + "| '_ \\| | | | '_ \\  " + end +
+    greyBg + " | |__| | (_| | || (_| " + yellowBg + "| | | | |_| | |_) | " + end +
+    greyBg + " |_____/ \\__,_|\\__\\__,_" + yellowBg + "|_| |_|\\__,_|_.__/  " + end +
+    greyBg + "                       " + yellowBg + "                    " + end
+
+
