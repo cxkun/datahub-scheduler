c75d823b63d6707ab95d0af81be247c60a974cf6
Jensen Qi <SuiterChik@gmail.com>
2020-01-20 23:53:56 +0800

    implement groups controller

--- src/main/kotlin/datahub/api/controller/GroupController.kt
+++ src/main/kotlin/datahub/api/controller/GroupController.kt
@@ -13,21 +13,85 @@
  */
 package datahub.api.controller
 
+import datahub.api.Response
+import datahub.api.ResponseData
+import datahub.api.utils.Page
+import datahub.dao.Groups
 import datahub.models.Group
+import me.liuwj.ktorm.dsl.eq
+import me.liuwj.ktorm.dsl.limit
+import me.liuwj.ktorm.dsl.select
+import me.liuwj.ktorm.dsl.where
+import me.liuwj.ktorm.entity.add
+import me.liuwj.ktorm.entity.findById
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+import javax.validation.constraints.NotBlank
 
+/**
+ * @author Jensen Qi
+ * @since 1.0.0
+ */
 @RestController
 @EnableAutoConfiguration
 @RequestMapping("/api/group")
 class GroupController {
 
-    @RequestMapping("listing")
-    @ResponseBody
-    fun listing(): List<Group> {
-        return listOf()
+    @GetMapping
+    fun listing(page: Int = 1, pageSize: Int = Int.MAX_VALUE): ResponseData {
+        val groups = Groups.select().where { Groups.isRemove eq false }
+        val count = groups.totalRecords
+        return Response.Success.WithData(mapOf(
+            "count" to count,
+            "groups" to groups.limit(Page.offset(page, pageSize), pageSize).map { Groups.createEntity(it) }
+        ))
+    }
+
+    @GetMapping("{id}")
+    fun find(@PathVariable id: Int): ResponseData {
+        val group = Groups.findById(id)
+        return if (group == null || group.isRemove) {
+            Response.Failed.DataNotFound("group $id")
+        } else {
+            Response.Success.WithData("group" to group)
+        }
+    }
+
+    @PostMapping
+    fun create(@NotBlank(message = "{required}") name: String): ResponseData {
+        val group = Group {
+            this.name = name
+            this.isRemove = false
+            this.createTime = LocalDateTime.now()
+            this.updateTime = LocalDateTime.now()
+        }
+        Groups.add(group)
+        return Response.Success.WithData("group" to group)
+    }
+
+    @PutMapping("{id}")
+    fun update(@PathVariable id: Int, name: String): ResponseData {
+        val group = Groups.findById(id)
+        return if (group == null || group.isRemove) {
+            Response.Failed.DataNotFound("group $id")
+        } else {
+            group.name = name
+            group.flushChanges()
+            Response.Success.WithData("group has been update")
+        }
+    }
+
+    @DeleteMapping("{id}")
+    fun remove(@PathVariable id: Int): ResponseData {
+        val group = Groups.findById(id)
+        return if (group == null || group.isRemove) {
+            Response.Failed.DataNotFound("group $id")
+        } else {
+            group.isRemove = true
+            group.flushChanges()
+            Response.Success.WithData("group has been remove")
+        }
     }
 
 }
