32f7045503c4c224005dc2b11a65950fafdf1c75
Jensen Qi <SuiterChik@gmail.com>
2020-02-01 22:44:47 +0800

    init random util and move postman to utils package

--- src/test/kotlin/datahub/tools/Postman.kt
+++ src/test/kotlin/datahub/tools/Postman.kt
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package datahub.api
+package datahub.tools
 
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.*
