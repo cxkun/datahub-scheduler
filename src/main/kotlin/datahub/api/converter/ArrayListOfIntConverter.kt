c74aabfd7a45a77b677b3492c9af63b8016efcac
Jensen Qi <SuiterChik@gmail.com>
2020-02-11 00:09:59 +0800

    move arraylist converter to converter package

--- src/main/kotlin/datahub/api/converter/ArrayListOfIntConverter.kt
+++ src/main/kotlin/datahub/api/converter/ArrayListOfIntConverter.kt
@@ -11,11 +11,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package datahub.api.component
+package datahub.api.converter
 
 import com.google.common.collect.Lists
-import com.google.common.collect.Sets
-import datahub.models.dtype.FileType
 import org.springframework.core.convert.converter.Converter
 import org.springframework.stereotype.Component
 
@@ -25,7 +23,7 @@ import org.springframework.stereotype.Component
  * @since 1.0.0
  */
 @Component
-class ArrayListOfIntCOnverter : Converter<String, ArrayList<Int>> {
+class ArrayListOfIntConverter : Converter<String, ArrayList<Int>> {
     override fun convert(value: String): ArrayList<Int> = Lists.newArrayList(
         value.trim('[', ']').split(",").map { it.trim().toInt() }
     )
