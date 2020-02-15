286e40f654bb411c34f2f5cd0e6ad289ea85c677
Jensen Qi <SuiterChik@gmail.com>
2020-02-15 23:24:55 +0800

    add PermissionDenied and IllegalArgument response

--- src/main/kotlin/datahub/api/Response.kt
+++ src/main/kotlin/datahub/api/Response.kt
@@ -37,7 +37,8 @@ object Response {
         const val status = "failed"
         fun WithError(error: Any) = withError(status, error)
         fun DataNotFound(data: Any) = withError(status, "$data not found")
-
+        fun PermissionDenied(message: Any) = withError(status, "permission denied: $message")
+        fun IllegalArgument(message: Any) = withData(status, "illegal argument: $message")
     }
 
 }
