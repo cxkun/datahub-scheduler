91eb36365fd86b52f68c517434ae58f4235e1eef
Jensen Qi <SuiterChik@gmail.com>
2020-02-25 23:52:54 +0800

    更新 Group Controller 单元测试的书写方式

--- src/test/kotlin/datahub/api/controller/GroupControllerTest.kt
+++ src/test/kotlin/datahub/api/controller/GroupControllerTest.kt
@@ -13,37 +13,20 @@
  */
 package datahub.api.controller
 
-import ch.vorburger.mariadb4j.DB
 import datahub.dao.SchemaUtils
 import datahub.models.dtype.FileType
 import datahub.tools.Postman
+import datahub.tools.RestfulTestToolbox
 import org.junit.jupiter.api.*
-import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpStatus
-import org.springframework.test.context.junit.jupiter.SpringExtension
 
 
 /**
  * @author Jensen Qi
  * @since 1.0.0
  */
-@ExtendWith(SpringExtension::class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Suppress("UNCHECKED_CAST")
-class GroupControllerTest {
-
-    @Autowired
-    lateinit var template: TestRestTemplate
-    private lateinit var postman: Postman
-
-    @BeforeAll
-    fun startDb(){
-        DB.newEmbeddedDB(3307).start()
-    }
+class GroupControllerTest : RestfulTestToolbox() {
 
     @BeforeEach
     fun initEnvironment() {
@@ -60,56 +43,28 @@ class GroupControllerTest {
     @Test
     fun listing() {
         val validGroupCount = 32
-        with(postman.get("/api/group")) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("success", body?.get("status"))
-            with(body?.get("data") as Map<String, Any>) {
-                Assertions.assertEquals(validGroupCount, get("count"))
-                val groups = get("groups") as List<LinkedHashMap<String, Any>>
-                Assertions.assertEquals(validGroupCount, groups.size)
-            }
-        }
+        postman.get("/api/group").shouldSuccess.thenGetData.andCheckCount(validGroupCount)
+            .thenGetListOf("groups").andCheckSize(validGroupCount)
 
         val pageSize = 7
         val queryTimes = validGroupCount / pageSize + 1
         val lastPageGroupCount = validGroupCount % pageSize
         for (page in 1..queryTimes) {
-            with(postman.get("/api/group", mapOf("page" to page, "pageSize" to pageSize))) {
-                Assertions.assertEquals(HttpStatus.OK, statusCode)
-                Assertions.assertEquals("success", body?.get("status"))
-                with(body?.get("data") as Map<String, Any>) {
-                    Assertions.assertEquals(validGroupCount, get("count"))
-                    val groups = get("groups") as List<LinkedHashMap<String, Any>>
-                    Assertions.assertEquals(if (page == queryTimes) lastPageGroupCount else pageSize, groups.size)
-                }
-            }
+            postman.get("/api/group", mapOf("page" to page, "pageSize" to pageSize)).shouldSuccess
+                .thenGetData.andCheckCount(validGroupCount)
+                .thenGetListOf("groups").andCheckSize(if (page == queryTimes) lastPageGroupCount else pageSize)
         }
     }
 
     @Test
     fun find() {
-        with(postman.get("/api/group/23")) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("success", body?.get("status"))
-            with(body?.get("data") as Map<String, Any>) {
-                val group = get("group") as LinkedHashMap<String, Any>
-                Assertions.assertEquals("kcwhynhd", group["name"])
-                Assertions.assertEquals("2044-11-11 15:27:26", group["createTime"])
-                Assertions.assertEquals("2047-07-02 20:28:57", group["updateTime"])
-            }
+        postman.get("/api/group/23").shouldSuccess.thenGetData.thenGetItem("group").withExpect {
+            it["name"] shouldBe "kcwhynhd"
+            it["createTime"] shouldBe "2044-11-11 15:27:26"
+            it["updateTime"] shouldBe "2047-07-02 20:28:57"
         }
-
-        with(postman.get("/api/group/39")) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("failed", body?.get("status"))
-            Assertions.assertEquals("group 39 not found", body?.get("error"))
-        }
-
-        with(postman.get("/api/group/40")) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("failed", body?.get("status"))
-            Assertions.assertEquals("group 40 not found", body?.get("error"))
-        }
+        postman.get("/api/group/39").shouldFailed.withError("group 39 not found")
+        postman.get("/api/group/40").shouldFailed.withError("group 40 not found")
     }
 
     @Test
@@ -119,84 +74,47 @@ class GroupControllerTest {
         val nextGroupId = 40
         val name = "test_create"
 
-        with(postman.post("/api/group", mapOf("name" to name))
-        ) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("success", body?.get("status"))
-            with(body?.get("data") as Map<String, Any>) {
-                with(get("group") as Map<String, Any>) {
-                    Assertions.assertEquals(nextGroupId, get("id"))
-                    Assertions.assertEquals(name, get("name"))
-                }
-                with(get("file") as Map<String, Any>) {
-                    Assertions.assertEquals(nextGroupId, get("groupId"))
-                    Assertions.assertEquals(2, get("ownerId"))
-                    Assertions.assertEquals(name, get("name"))
-                    Assertions.assertEquals(FileType.DIR.toString(), get("type"))
-                    Assertions.assertEquals(null, get("version"))
-                    Assertions.assertEquals(null, get("parentId"))
-                }
+        postman.post("/api/group", mapOf("name" to name)).shouldSuccess.thenGetData.apply {
+            this.thenGetItem("group").withExpect {
+                it["id"] shouldBe nextGroupId
+                it["name"] shouldBe name
+            }
+            this.thenGetItem("file").withExpect {
+                it["groupId"] shouldBe nextGroupId
+                it["ownerId"] shouldBe 2
+                it["name"] shouldBe name
+                it["type"] shouldBe FileType.DIR.toString()
+                it["version"] shouldBe null
+                it["parentId"] shouldBe null
             }
         }
 
-        with(postman.get("/api/group/$nextGroupId")) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("success", body?.get("status"))
-            with(body?.get("data") as Map<String, Any>) {
-                val group = get("group") as LinkedHashMap<String, Any>
-                Assertions.assertEquals(name, group["name"])
-            }
+        postman.get("/api/group/$nextGroupId").shouldSuccess.thenGetData.thenGetItem("group").withExpect {
+            it["name"] shouldBe name
         }
 
         // todo test file created
-
     }
 
     @Test
     fun update() {
         val newName = "new_name"
-
-        with(postman.put("/api/group/25", mapOf("name" to newName))) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("success", body?.get("status"))
-            Assertions.assertEquals("group 25 has been update", body?.get("message"))
-        }
-
-        with(postman.get("/api/group/25")) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("success", body?.get("status"))
-            with(body?.get("data") as Map<String, Any>) {
-                val group = get("group") as LinkedHashMap<String, Any>
-                Assertions.assertEquals(newName, group["name"])
-                Assertions.assertNotEquals("2051-07-10 20:16:48", group["updateTime"])
-            }
+        postman.put("/api/group/25", mapOf("name" to newName)).shouldSuccess.withMessage("group 25 has been update")
+        postman.get("/api/group/25").shouldSuccess.thenGetData.thenGetItem("group").withExpect {
+            it["name"] shouldBe newName
+            it["updateTime"] shouldNotBe "2051-07-10 20:16:48"
         }
     }
 
     @Test
     fun remove() {
-        with(postman.delete("/api/group/15")) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("success", body?.get("status"))
-            Assertions.assertEquals("group 15 has been removed", body?.get("message"))
-        }
+        postman.get("/api/group/15").shouldSuccess
+        postman.delete("/api/group/15").shouldSuccess.withMessage("group 15 has been removed")
+        postman.get("/api/group/15").shouldFailed.withError("group 15 not found")
 
-        with(postman.get("/api/group/15")) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("failed", body?.get("status"))
-            Assertions.assertEquals("group 15 not found", body?.get("error"))
-        }
+        postman.delete("/api/group/18").shouldFailed.withError("group 18 not found")
 
-        with(postman.delete("/api/group/18")) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("failed", body?.get("status"))
-            Assertions.assertEquals("group 18 not found", body?.get("error"))
-        }
+        postman.delete("/api/group/40").shouldFailed.withError("group 40 not found")
 
-        with(postman.delete("/api/group/40")) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("failed", body?.get("status"))
-            Assertions.assertEquals("group 40 not found", body?.get("error"))
-        }
     }
 }
