f675ce11442737e36541f3c00ec026b4b85fbe9f
Jensen Qi <SuiterChik@gmail.com>
2020-02-26 00:02:17 +0800

    更新 Login Controller 单元测试书写方式

--- src/test/kotlin/datahub/api/controller/LoginControllerTest.kt
+++ src/test/kotlin/datahub/api/controller/LoginControllerTest.kt
@@ -13,35 +13,17 @@
  */
 package datahub.api.controller
 
-import ch.vorburger.mariadb4j.DB
 import datahub.tools.Postman
 import datahub.api.auth.Jwt
 import datahub.dao.SchemaUtils
+import datahub.tools.RestfulTestToolbox
 import org.junit.jupiter.api.*
-import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.http.HttpStatus
-import org.springframework.test.context.junit.jupiter.SpringExtension
 
 /**
  * @author Jensen Qi
  * @since 1.0.0
  */
-@ExtendWith(SpringExtension::class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@Suppress("UNCHECKED_CAST")
-class LoginControllerTest {
-    @Autowired
-    lateinit var template: TestRestTemplate
-    private lateinit var postman: Postman
-
-    @BeforeAll
-    fun startDb() {
-        DB.newEmbeddedDB(3307).start()
-    }
+class LoginControllerTest : RestfulTestToolbox() {
 
     @BeforeEach
     fun rebuildDB() {
@@ -52,33 +34,14 @@ class LoginControllerTest {
 
     @Test
     fun login() {
-        with(postman.post("/api/login", mapOf(
-            "username" to "root",
-            "password" to "root"
-        ))) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("success", body?.get("status"))
-            with(body?.get("data") as Map<String, String>) {
-                Assertions.assertEquals("root", Jwt.getUserName(get("token") ?: ""))
-            }
-        }
+        val token = postman.post("/api/login", mapOf("username" to "root", "password" to "root")).shouldSuccess
+            .thenGetData["token"].toString()
+        Assertions.assertEquals("root", Jwt.getUserName(token))
 
-        with(postman.post("/api/login", mapOf(
-            "username" to "root",
-            "password" to "wrong password"
-        ))) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("failed", body?.get("status"))
-            Assertions.assertEquals("login failed", body?.get("error"))
-        }
+        postman.post("/api/login", mapOf("username" to "root", "password" to "wrong password"))
+            .shouldFailed.withError("login failed")
 
-        with(postman.post("/api/login", mapOf(
-            "username" to "wrong username",
-            "password" to "root"
-        ))) {
-            Assertions.assertEquals(HttpStatus.OK, statusCode)
-            Assertions.assertEquals("failed", body?.get("status"))
-            Assertions.assertEquals("login failed", body?.get("error"))
-        }
+        postman.post("/api/login", mapOf("username" to "wrong username", "password" to "root"))
+            .shouldFailed.withError("login failed")
     }
 }
