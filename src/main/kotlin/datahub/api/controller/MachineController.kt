9740aa775c25753debb2468ac7cf23deeb5c02db
Jensen Qi <SuiterChik@gmail.com>
2020-03-13 01:05:22 +0800

    实现调度机器的增删改查接口，并添加单元测试

--- src/main/kotlin/datahub/api/controller/MachineController.kt
+++ src/main/kotlin/datahub/api/controller/MachineController.kt
@@ -13,21 +13,177 @@
  */
 package datahub.api.controller
 
+import com.google.common.collect.Lists
+import datahub.api.Response
+import datahub.api.ResponseData
+import datahub.api.utils.Page
+import datahub.dao.Machines
 import datahub.models.Machine
+import me.liuwj.ktorm.dsl.*
+import me.liuwj.ktorm.entity.add
+import me.liuwj.ktorm.entity.findById
+import me.liuwj.ktorm.schema.ColumnDeclaring
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
 
+/**
+ * @author Jensen Qi
+ * @since 1.0.0
+ */
 @RestController
 @EnableAutoConfiguration
 @RequestMapping("/api/machine")
 class MachineController {
 
-    @RequestMapping("listing")
-    @ResponseBody
-    fun listing(): List<Machine> {
-        return listOf()
+    /**
+     * @api {get} /api/machine 获取机器列表
+     * @apiDescription 获取机器列表，支持分页查询和模糊查询
+     * @apiGroup Machine
+     * @apiVersion 0.1.0
+     * @apiHeader {String} token 用户授权 token
+     * @apiParam {Number} [page = 1] 分页ID
+     * @apiParam {Number} [pageSize = 9999] 分页大小
+     * @apiParam {String} like 机器 hostname 模糊匹配，多个词用空格分隔，null 字符串会被忽
+     * @apiSuccessExample 请求成功
+     * {"status":"success","data":{"count":188,"machines":[{"id":234,"hostname":"hevhjzva","mac":"50-95-F4-68-0E-DA","ip":"145.28.32.35","cpuLoad":34,"memLoad":2,"diskUsage":80,"isRemove":false,"createTime":"2009-07-03 10:34:56","updateTime":"2010-12-22 20:37:10"},{"id":235,"hostname":"djesrwkr","mac":"1F-E1-DA-20-DB-EB","ip":"95.127.187.33","cpuLoad":13,"memLoad":27,"diskUsage":35,"isRemove":false,"createTime":"2019-07-15 22:29:11","updateTime":"2019-12-13 07:56:14"}]}}
+     * @apiSuccessExample 请求失败
+     * {"status":"failed","error":"错误信息"}
+     */
+    @GetMapping
+    fun listing(@RequestParam(required = false, defaultValue = "1") page: Int,
+                @RequestParam(required = false, defaultValue = "9999") pageSize: Int,
+                @RequestParam(required = false) like: String?): ResponseData {
+        val machines = Machines.select().where {
+            val conditions = Lists.newArrayList<ColumnDeclaring<Boolean>>(Machines.isRemove eq false)
+            if (like != null && like.isNotBlank() && like.trim().toUpperCase() != "NULL") {
+                like.split("\\s+".toRegex()).forEach {
+                    conditions.add(Machines.hostname.like("%$it%"))
+                }
+            }
+            conditions.reduce { a, b -> a and b }
+        }
+        val count = machines.totalRecords
+        return Response.Success.WithData(mapOf(
+            "count" to count,
+            "machines" to machines.orderBy(Machines.id.asc()).limit(Page.offset(page, pageSize), pageSize).map {
+                Machines.createEntity(it)
+            }
+        ))
+    }
+
+    /**
+     * @api {get} /api/machine/{id} 查询机器
+     * @apiDescription 查询指定 ID 的机器详情
+     * @apiGroup Machine
+     * @apiVersion 0.1.0
+     * @apiHeader {String} token 用户授权 token
+     * @apiSuccessExample 请求成功
+     * {"status":"success","data":{"machine":{"id":1,"hostname":"tejxajfq","mac":"1F-72-5B-F7-10-AB","ip":"107.116.90.29","cpuLoad":34,"memLoad":31,"diskUsage":63,"isRemove":false,"createTime":"2029-06-06 19:57:08","updateTime":"2032-06-08 19:36:03"}}}
+     * @apiSuccessExample 请求失败
+     * {"status":"failed","error":"machine 2 not found"}
+     */
+    @GetMapping("{id}")
+    fun find(@PathVariable id: Int): ResponseData {
+        val machine = Machines.findById(id)
+        return if (machine == null || machine.isRemove) {
+            Response.Failed.DataNotFound("machine $id")
+        } else {
+            Response.Success.WithData(mapOf("machine" to machine))
+        }
+    }
+
+    /**
+     * @api {post} /api/machine 新建机器
+     * @apiDescription 新建一台调度服务器，并将新建后的信息返回
+     * @apiGroup Machine
+     * @apiVersion 0.1.0
+     * @apiHeader {String} token 用户授权 token
+     * @apiParam {String} hostname 服务器 hostname
+     * @apiParam {String} ip 服务器 IP
+     * @apiSuccessExample 请求成功
+     * {"status":"success","data":{"machine":{"hostname":"test","mac":"","ip":"192.168.1.1","cpuLoad":0,"memLoad":0,"diskUsage":0,"isRemove":false,"createTime":"2020-03-13 00:42:16","updateTime":"2020-03-13 00:42:16","id":248}}}
+     * @apiSuccessExample 请求失败
+     * {"status":"failed","error":"错误信息"}
+     */
+    @PostMapping
+    fun create(@RequestParam(required = true) hostname: String,
+               @RequestParam(required = true) ip: String): ResponseData {
+        val machine = Machine {
+            this.hostname = hostname
+            this.mac = ""
+            this.ip = ip
+            this.cpuLoad = 0
+            this.memLoad = 0
+            this.diskUsage = 0
+            this.isRemove = false
+            this.createTime = LocalDateTime.now()
+            this.updateTime = LocalDateTime.now()
+        }
+        Machines.add(machine)
+        return Response.Success.WithData(mapOf("machine" to machine))
+    }
+
+    /**
+     * @api {put} /api/machine/{id} 更新机器信息
+     * @apiDescription 更新指定 ID 的机器信息，如果该指定机器已被删除或不存在则失败
+     * @apiGroup Machine
+     * @apiVersion 0.1.0
+     * @apiHeader {String} token 用户授权 token
+     * @apiParam {String} [hostname = null] 机器 hostname，如果不提供则不更新
+     * @apiParam {String} [ip = null] 机器 IP，如果不提供则不更新
+     * @apiSuccessExample 请求成功
+     * {"status":"success","data":{"machine":{"id":5,"hostname":"myjwey","mac":"7D-75-70-DE-73-0E","ip":"14.66.49.193","cpuLoad":1,"memLoad":60,"diskUsage":59,"isRemove":false,"createTime":"2004-11-28 21:50:06","updateTime":"2020-03-13 01:01:13"}}}
+     * @apiSuccessExample 请求失败
+     * {"status":"failed","error":"machine 2 not found"}
+     */
+    @PutMapping("{id}")
+    fun update(@PathVariable id: Int, @RequestParam(required = false) hostname: String?,
+               @RequestParam(required = false) ip: String?): ResponseData {
+        val machine = Machines.findById(id)
+        return if (machine == null || machine.isRemove) {
+            Response.Failed.DataNotFound("machine $id")
+        } else {
+            var update = false
+            if (hostname != null) {
+                machine.hostname = hostname
+                update = true
+            }
+            if (ip != null) {
+                machine.ip = ip
+                update = true
+            }
+            if (update) {
+                machine.updateTime = LocalDateTime.now()
+            }
+            machine.flushChanges()
+            Response.Success.WithData(mapOf("machine" to machine))
+        }
+
+    }
+
+    /**
+     * @api {delete} /api/machine/{id} 删除机器
+     * @apiDescription 删除指定 ID 的机器，如果该指定机器已被删除或不存在则失败
+     * @apiGroup Machine
+     * @apiVersion 0.1.0
+     * @apiHeader {String} token 用户授权 token
+     * @apiSuccessExample 请求成功
+     * {"status":"success","message":"machine 3 has been removed"}
+     * @apiSuccessExample 请求失败
+     * {"status":"failed","error":"machine 2 not found"}
+     */
+    @DeleteMapping("{id}")
+    fun remove(@PathVariable id: Int): ResponseData {
+        val machine = Machines.findById(id)
+        return if (machine == null || machine.isRemove) {
+            Response.Failed.DataNotFound("machine $id")
+        } else {
+            machine.isRemove = true
+            machine.flushChanges()
+            Response.Success.Remove("machine ${machine.id}")
+        }
+
     }
 
 }
--- src/test/kotlin/datahub/api/controller/MachineControllerTest.kt
+++ src/test/kotlin/datahub/api/controller/MachineControllerTest.kt
@@ -1,5 +1,153 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package datahub.api.controller
 
-import org.junit.Assert.*
+import datahub.dao.SchemaUtils
+import datahub.tools.Postman
+import datahub.tools.RestfulTestToolbox
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
-class MachineControllerTest
+
+class MachineControllerTest : RestfulTestToolbox() {
+
+    @BeforeEach
+    fun rebuildDB() {
+        SchemaUtils.rebuildDB()
+        SchemaUtils.loadTable("datahub.machines", this.javaClass.classLoader.getResource("tables/machines.txt")!!.path)
+        SchemaUtils.loadTable("datahub.users", this.javaClass.classLoader.getResource("tables/users.txt")!!.path)
+        this.postman = Postman(template)
+        this.postman.login()
+    }
+
+    @Test
+    fun listing() {
+        val validMachineCount = 188
+        postman.get("/api/machine").shouldSuccess.thenGetData.andCheckCount(validMachineCount)
+            .thenGetListOf("machines").andCheckSize(validMachineCount)
+
+        val pageSize = 13
+        val queryTimes = validMachineCount / pageSize + 1
+        val lastPageMachineCount = validMachineCount % pageSize
+        for (page in 1..queryTimes) {
+            postman.get("/api/machine", mapOf("page" to page, "pageSize" to pageSize)).shouldSuccess
+                .thenGetData.andCheckCount(validMachineCount)
+                .thenGetListOf("machines").andCheckSize(if (page == queryTimes) lastPageMachineCount else pageSize)
+        }
+    }
+
+    @Test
+    fun search() {
+        // 提供空或 null 的相似词
+        var validMachineCount = 188
+        var pageSize = 13
+        var queryTimes = validMachineCount / pageSize + 1
+        var lastPageMachineCount = validMachineCount % pageSize
+        for (page in 1..queryTimes) {
+            postman.get("/api/machine", mapOf("page" to page, "pageSize" to pageSize, "like" to null)).shouldSuccess
+                .thenGetData.andCheckCount(validMachineCount)
+                .thenGetListOf("machines").andCheckSize(if (page == queryTimes) lastPageMachineCount else pageSize)
+
+            postman.get("/api/machine", mapOf("page" to page, "pageSize" to pageSize, "like" to "  ")).shouldSuccess
+                .thenGetData.andCheckCount(validMachineCount)
+                .thenGetListOf("machines").andCheckSize(if (page == queryTimes) lastPageMachineCount else pageSize)
+        }
+
+        // 提供 1 个相似词
+        validMachineCount = 58
+        pageSize = 13
+        queryTimes = validMachineCount / pageSize + 1
+        lastPageMachineCount = validMachineCount % pageSize
+        for (page in 1..queryTimes) {
+            postman.get("/api/machine", mapOf("page" to page, "pageSize" to pageSize, "like" to " a ")).shouldSuccess
+                .thenGetData.andCheckCount(validMachineCount)
+                .thenGetListOf("machines").andCheckSize(if (page == queryTimes) lastPageMachineCount else pageSize)
+        }
+
+        // 提供 2 个相似词
+        validMachineCount = 15
+        pageSize = 4
+        queryTimes = validMachineCount / pageSize + 1
+        lastPageMachineCount = validMachineCount % pageSize
+        for (page in 1..queryTimes) {
+            postman.get("/api/machine", mapOf("page" to page, "pageSize" to pageSize, "like" to " a b")).shouldSuccess
+                .thenGetData.andCheckCount(validMachineCount)
+                .thenGetListOf("machines").andCheckSize(if (page == queryTimes) lastPageMachineCount else pageSize)
+        }
+    }
+
+    @Test
+    fun create() {
+        val newHostname = "new host name"
+        val newIP = "192.168.1.1"
+        val nextId = 247
+
+        postman.post("/api/machine", mapOf("hostname" to newHostname, "ip" to newIP)).shouldSuccess.thenGetData
+            .thenGetItem("machine").withExpect {
+                it["id"] shouldBe nextId
+                it["hostname"] shouldBe newHostname
+                it["ip"] shouldBe newIP
+                it["mac"] shouldBe ""
+                it["cpuLoad"] shouldBe 0
+                it["memLoad"] shouldBe 0
+                it["diskUsage"] shouldBe 0
+            }
+    }
+
+    @Test
+    fun update() {
+        val newHostname = "new host name"
+        val newIP = "192.168.1.1"
+
+        // 只更新 hostname
+        postman.put("/api/machine/1", mapOf("hostname" to newHostname)).shouldSuccess.thenGetData
+            .thenGetItem("machine").withExpect {
+                it["hostname"] shouldBe newHostname
+                it["ip"] shouldBe "107.116.90.29"
+                it["updateTime"] shouldNotBe "2032-06-08 19:36:03"
+            }
+
+        // 只更新 IP
+        postman.put("/api/machine/3", mapOf("ip" to newIP)).shouldSuccess.thenGetData
+            .thenGetItem("machine").withExpect {
+                it["hostname"] shouldBe "nknvleif"
+                it["ip"] shouldBe newIP
+                it["updateTime"] shouldNotBe "2036-03-31 18:40:59"
+            }
+
+        // 更新 hostname, IP
+        postman.put("/api/machine/5", mapOf("hostname" to newHostname, "ip" to newIP)).shouldSuccess.thenGetData
+            .thenGetItem("machine").withExpect {
+                it["hostname"] shouldBe newHostname
+                it["ip"] shouldBe newIP
+                it["updateTime"] shouldNotBe "2006-05-14 03:39:26"
+            }
+
+        // 更新已删除的机器信息
+        postman.put("/api/machine/2", mapOf("hostname" to newHostname, "ip" to newIP))
+            .shouldFailed.withNotFoundError("machine 2")
+
+        // 更新不存在的机器信息
+        postman.put("/api/machine/247", mapOf("hostname" to newHostname, "ip" to newIP))
+            .shouldFailed.withNotFoundError("machine 247")
+
+    }
+
+    @Test
+    fun remove() {
+        postman.delete("/api/machine/11").shouldSuccess.withMessage("machine 11 has been removed")
+        postman.delete("/api/machine/12").shouldFailed.withNotFoundError("machine 12") // 已删除的
+        postman.delete("/api/machine/247").shouldFailed.withNotFoundError("machine 247") // 不存在的
+    }
+}
